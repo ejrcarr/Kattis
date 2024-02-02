0cfcadf525a0cb5ddfca8337e809a4f55bef466d
Evan Carr <ejrcarr@gmail.com>
2024-02-02 11:13:58 -0500

    [C++] Add solution for Solving for Carrots

---
+++ Solutions/Solving for Carrots/carrots.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define sz(x) (int)(x).size()
+typedef vector<int> vi;
+
+int main() {
+    int n, p;
+    cin >> n >> p;
+    cout << p;
+}
